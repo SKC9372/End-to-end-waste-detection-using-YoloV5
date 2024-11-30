0e068c150cc3cac1b8d30993af17f4df84c7b702
SKC9372 <suryakantchaubey2001@gmail.com>
2024-11-30 15:11:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="waste_detection",
+    author_email="suryakantchaubey2001@gmail.com",
+    packages=find_packages(),
+    version='0.0.1',
+    install_requires=[],
+    author="Suryakant Chaubey"
+)
